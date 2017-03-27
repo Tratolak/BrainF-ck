6939cc7043c3628428fde55864196f6a3994cbe8
m44615 <martin.krbila@volny.cz>
2017-03-27 14:28:37 +0200

    Code blocks + modul na optimalizaci

---
+++ optimizer.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include "optimizer.h"
+
+int optimize(unsigned char *code,int *jumpTable,int len)
+{
+    return 0;
+}
---
+++ optimizer.h
@@ -0,0 +1,6 @@
+#ifndef OTPIMIZER_INCLUDED
+#define OTPIMIZER_INCLUDED
+
+int optimize(unsigned char *code,int *jumpTable,int len);
+
+#endif // OTPIMIZER_INCLUDED
